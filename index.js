3fd26164d3a3b994cf763ba0ae05eaae5e6c5823
Emmanuel <zacmac807@gmail.com>
2024-03-27 12:48:44 +0300

    add file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
 const farmAnimals = 'cow horse sheep pig chicken';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -10,6 +12,7 @@ const muppet = {
   partner: 'Kermit'
 };
 
+//object
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -28,21 +31,37 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const animalNames = 'cow sheep pig chicken';
+const [bessie, dolly, babe, little] = animalNames.split(' ');
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+const [blackAndWhite = 'cow', , black = 'sheep', pink = 'pig'] =farmAnimals.split(' ');
+
 
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
+const  [red,orange, yellow, green, blue, indigo, violet] = colors
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
+    const [r, o ,y ,g , b,,v] = colors
+    
+
+
+
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+const [,,,,,indg,] = colors;
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+const { muppetName, color, song, job, partner } = muppet;
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { album: { theMuppetMovie: { song2, song4 } }, nestedJob, nestedPartner } = nestedMuppet;
+//const { song2, song4} =album.nestedMuppet.theMuppetMovie ;
+// const { nestedJob, nestedPartner } = nestedMuppet;
+
